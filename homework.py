52ec4bdee56f4a09ae86f3b800d8fd9cde480a7d
Noah Gaeckle <noahgaeckle@gmail.com>
2023-02-16 18:01:31 -0600

    second commit

--- homework.py
+++ homework.py
@@ -1,57 +1,33 @@
-
-class AddressBook:
+class Cart():
     def __init__(self):
-        self.contacts = []
-        
-    def add_contact(self):
-        name = input('Enter Name: ')
-        phone_number = input('Enter Phone Number: ')
-        address = input('Enter Address: ')
-        email = input('Enter Email: ')
-        
-        new_contact = Contact(name, phone_number, address, email)
-        self.contacts.append(new_contact)
-        
-    def delete_contact(self):
-        delete_name = input('Enter name to delete: ')
-        
-        for i in range(len(self.contacts)):
-            if self.contacts[i].name.lower() == delete_name.lower():
-                self.contacts.pop(i)
-                print('Contact Deleted')
-                return
-            
-        print(f'{delete_name} was not found.')
-        
-    def print_contacts(self):
-        for contact in self.contacts:
-            print(f'{contact.name}')
-            print(f'Phone Number: {contact.phone_number}')
-            print(f'Address: {contact.address}')
-            print(f'Email: {contact.email}')
-            
-    def run(self):
-        while True:
-            user_choice = input('What would you like to do? (add/delete/show/quit)').lower()
-            
-            if user_choice == 'add':
-                self.add_contact()
-            elif user_choice == 'delete':
-                self.delete_contact()
-            elif user_choice == 'show':
-                self.print_contacts()
-            elif user_choice == 'quit':
-                self.print_contacts()
-                return
-            else:
-                print('That was not valid input, please try again.')
-    
-class Contact:
-    def __init__(self, name, phone_number, address, email):
-        self.name = name
-        self.phone_number = phone_number
-        self.address = address
-        self.email = email
+        self.inventory = []
+
+
+    def add_info(self, item):
+        self.inventory.append(item)
+
+    def del_info(self, item):
+        self.inventory.remove(item)
+
+    def show_info(self):
+        print(self.inventory)
+my_cart = Cart()
+def main():
+    while True:
+        choice = input("What would you like to do? Show/Add/Delete or Quit? ")
+        if choice.lower() == "show":
+            my_cart.show_info()
+        elif choice.lower() == "add":
+            item = input("what would you like to add? ")
+            my_cart.add_info(item)
+        elif choice.lower() == "delete":
+            item = input("What would you like to delete? ")
+            my_cart.del_info(item)
+        else:
+            print("You chose to quit")
+            break
+    my_cart.show_info()
+main()
         
 
 """
